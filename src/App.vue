859f05da719cf0f26c2489d9f42814e5d14849be
Vadim <lostprophetsco@gmail.com>
2024-10-21 19:30:31 +0300

    feat: add button styles

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,13 @@
-<template></template>
-<script setup lang="ts"></script>
+<template>
+  <Button>Test</Button>
+
+  <Button modifier="rounded">
+    <template #icon></template>
+  </Button>
+
+  <Button disabled>Test</Button>
+</template>
+<script setup lang="ts">
+// Imports
+import Button from './components/atoms/buttons/button.vue';
+</script>
