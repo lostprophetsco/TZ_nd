6727494377b1417958ca5f5ee84d2a2bc66105e3
Vadim <lostprophetsco@gmail.com>
2024-10-21 20:40:53 +0300

    feat: add svg sprites

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <Button>Test</Button>
+  <Button
+    >Вход
+    <template #icon>
+      <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
+        <use :href="`#${iconLogin}`" />
+      </svg>
+    </template>
+  </Button>
 
   <Button modifier="rounded">
-    <template #icon></template>
+    Вход
+    <template #icon>
+      <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+        <use :href="`#${iconAdd}`" />
+      </svg>
+    </template>
   </Button>
 
   <Button disabled>Test</Button>
 </template>
 <script setup lang="ts">
 // Imports
+import iconAdd from './assets/images/icons/add.svg';
+import iconLogin from './assets/images/icons/login.svg';
+
 import Button from './components/atoms/buttons/button.vue';
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    createSvgSpritePlugin({
+      symbolId: 'icon-[name]-[hash]',
+      include: ['**/src/assets/images/icons/**/*.svg'],
+    }),
+  ],
+});
